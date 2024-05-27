832c61a7fc2165bfaaaa44f316400e51f29bfc33
ZihengXu <ziheng.xu@nxp.com>
2024-05-27 16:02:51 +0900

    update license and SCR

--- rsync_to_imx93.sh
+++
@@ -1,6 +0,0 @@
-#
-# Copyright 2024 NXP
-#
-# SPDX-License-Identifier: BSD-3-Clause
-
-rsync -rlptDvz --progress --exclude-from=./exclude.list -e "ssh" --rsync-path=/usr/local/bin/rsync ../ETA_sys/ root@10.193.102.186:/home/root/NetworkingML
